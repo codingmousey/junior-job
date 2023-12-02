49aeef154f002e55b6ba574bf0b4aa5ca35a6698
codingmousey <daliniellee@gmail.com>
2023-12-02 15:21:58 +0100

    spring boot first commit

---
+++ src/main/java/ali/trabi/juniorjob/JuniorJobApplication.java
@@ -0,0 +1,13 @@
+package ali.trabi.juniorjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ali/trabi/juniorjob/JuniorJobApplicationTests.java
@@ -0,0 +1,13 @@
+package ali.trabi.juniorjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
